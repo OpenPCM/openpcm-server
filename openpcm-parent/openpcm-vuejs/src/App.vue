924d1d4078056aef136cedb423df4d34193b609e
Raymond King <gsugambit@gmail.com>
2018-03-04 04:09:41 -0600

    added stethoscope for image

--- openpcm-parent/openpcm-vuejs/src/App.vue
+++ openpcm-parent/openpcm-vuejs/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-    <img src="./assets/logo.png">
+    <img src="./assets/stethoscope.png">
     <h1>OpenPCM</h1>
     <h2>Open Patient Care Management</h2>
     <p>Coming soon...</p>
