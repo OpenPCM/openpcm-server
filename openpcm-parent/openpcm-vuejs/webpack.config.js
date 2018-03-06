72f0a920ddec0a58b6af83e8be43c7d84e6d14a1
aherrera <zim_789@yahoo.com>
2018-03-06 01:26:25 -0600

    changed code to show up as a cleaner modal for Add Patient (#6)
    excellent work, nice way to switch the modal

--- openpcm-parent/openpcm-vuejs/webpack.config.js
+++ openpcm-parent/openpcm-vuejs/webpack.config.js
@@ -16,11 +16,16 @@ module.exports = {
           'vue-style-loader',
           'css-loader'
         ],
-      },      {
+      }, {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
           loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this necessary.
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
           // other vue-loader options go here
         }
@@ -36,6 +41,19 @@ module.exports = {
         options: {
           name: '[name].[ext]?[hash]'
         }
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'less-loader'
+        ]
       }
     ]
   },
