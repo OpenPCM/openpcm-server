ccf2ef3e8f3a552e6a17435685e804976c68c954
Raymond King <gsugambit@gmail.com>
2018-03-18 15:34:55 -0500

    Updating FileStoreController to handle download

--- openpcm-server-parent/openpcm-server/src/main/java/org/openpcm/controller/FileStorageController.java
+++ openpcm-server-parent/openpcm-server/src/main/java/org/openpcm/controller/FileStorageController.java
@@ -3,8 +3,13 @@ package org.openpcm.controller;
 import java.io.IOException;
 
 import org.openpcm.exceptions.FileEmptyException;
+import org.openpcm.exceptions.NotFoundException;
 import org.openpcm.service.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +25,7 @@ import io.swagger.annotations.ApiResponses;
 @RestController
 @Api(value = "/api/v1/")
 @RequestMapping("/api/v1/")
-public class FileUploadController extends BaseController {
+public class FileStorageController extends BaseController {
 
 	@Autowired
 	private FileStorageService service;
@@ -32,6 +37,19 @@ public class FileUploadController extends BaseController {
 			@ApiParam(value = "file to upload", name = "file", required = true) @RequestParam("file") MultipartFile file)
 			throws FileEmptyException, IOException {
 		service.storeFile(file);
+	}
+
+	@ApiOperation(value = "downloadFile")
+	@PostMapping(value = "download/{filename}")
+	@ApiResponses({ @ApiResponse(code = 200, message = "successfully downloaded file") })
+	public ResponseEntity<Resource> downloadFile(
+			@ApiParam(value = "filename to download", name = "filename", required = true) @PathVariable("filename") String filename)
+			throws FileEmptyException, IOException, NotFoundException {
+		Resource file = service.loadAsResource(filename);
+
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+				.body(file);
 	}
 
 }
--- openpcm-server-parent/openpcm-server/src/main/java/org/openpcm/service/FileStorageService.java
+++ openpcm-server-parent/openpcm-server/src/main/java/org/openpcm/service/FileStorageService.java
@@ -1,14 +1,18 @@
 package org.openpcm.service;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.openpcm.exceptions.FileEmptyException;
+import org.openpcm.exceptions.NotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,8 +21,14 @@ public class FileStorageService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(FileStorageService.class);
 
-	@Value("${openpcm.fileStorage:./target/}")
-	private String fileStorage;
+	private final String fileStorage;
+
+	private final Path rootLocation;
+
+	public FileStorageService(@Value("${openpcm.fileStorage:./target/}") String fileStorage) {
+		this.fileStorage = fileStorage;
+		this.rootLocation = Paths.get(fileStorage);
+	}
 
 	public void storeFile(MultipartFile file) throws FileEmptyException, IOException {
 		if (file.isEmpty()) {
@@ -30,7 +40,21 @@ public class FileStorageService {
 		Files.write(path, bytes);
 
 		LOGGER.debug("Successfully uploaded: {}. {} bytes", file.getOriginalFilename(), file.getSize());
+	}
 
+	public Resource loadAsResource(String filename) throws NotFoundException {
+		try {
+			Path file = rootLocation.resolve(filename);
+			Resource resource = new UrlResource(file.toUri());
+			if (resource.exists() || resource.isReadable()) {
+				return resource;
+			} else {
+				throw new NotFoundException("Could not read file: " + filename);
+
+			}
+		} catch (MalformedURLException e) {
+			throw new NotFoundException("Could not read file: " + filename, e);
+		}
 	}
 
 }
