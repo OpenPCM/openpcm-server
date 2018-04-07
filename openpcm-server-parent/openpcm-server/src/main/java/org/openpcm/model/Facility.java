fcbd0911561e4ce8798616c0b9e2271c8d0027f9
Raymond King <gsugambit@gmail.com>
2018-04-07 05:43:16 -0500

    adding facility and room support for testing new CD pipeline

---
+++ openpcm-server-parent/openpcm-server/src/main/java/org/openpcm/model/Facility.java
@@ -0,0 +1,57 @@
+package org.openpcm.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.openpcm.utils.ObjectUtil;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "facility")
+public class Facility {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "facility_id")
+	private Long id;
+
+	@NotNull
+	@Column(unique = true)
+	private String name;
+
+	/** The address. */
+	@Embedded
+	private Address address;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "facility_room", joinColumns = @JoinColumn(name = "facility_id"), inverseJoinColumns = @JoinColumn(name = "room_id"))
+	private Set<Room> rooms;
+
+	@Override
+	public String toString() {
+		return ObjectUtil.print(this);
+	}
+
+}
