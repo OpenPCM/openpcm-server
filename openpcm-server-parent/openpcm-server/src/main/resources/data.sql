a71f8e1a21741c559ea3a656174c73c5c298c6bb
Raymond King <gsugambit@gmail.com>
2018-04-08 23:23:40 -0500

    updating id's to avoid hibernate exception on save, will have to find permanent solution

--- openpcm-server-parent/openpcm-server/src/main/resources/data.sql
+++ openpcm-server-parent/openpcm-server/src/main/resources/data.sql
@@ -1,22 +1,22 @@
 /* Inserting the default roles */
 INSERT INTO role (role_id, name)
-VALUES (1, 'ROLE_ADMIN');
+VALUES (14505, 'ROLE_ADMIN');
 
 INSERT INTO role (role_id, name)
-VALUES (2, 'ROLE_USER');
+VALUES (14506, 'ROLE_USER');
 
 INSERT INTO role (role_id, name)
-VALUES (3, 'ROLE_DOCTOR');
+VALUES (14507, 'ROLE_DOCTOR');
 
 INSERT INTO role (role_id, name)
-VALUES (4, 'ROLE_NURSE');
+VALUES (14508, 'ROLE_NURSE');
 
 INSERT INTO role (role_id, name)
-VALUES (5, 'ROLE_PATIENT');
+VALUES (14509, 'ROLE_PATIENT');
 
 /* Inserting default admin account */
 INSERT INTO user (address_line_one, address_line_two, city, state, zip_code, date_of_birth, email, enabled, first_name, gender, last_name, maiden_name, middle_name, mrn, password, phone_number, ssn, username, user_id)
-VALUES (NULL,NULL,NULL,NULL,NULL,NULL,'openpcm@gmail.com',true,'Admin',NULL,'User',NULL,NULL,NULL,'$2a$10$sfx.zurap9dhYL8fyb9Y.O82EBwIAhODSJJk9g7XoyY2vbfUpgzm2',NULL,NULL,'admin',1);
+VALUES (NULL,NULL,NULL,NULL,NULL,NULL,'openpcm@gmail.com',true,'Admin',NULL,'User',NULL,NULL,NULL,'$2a$10$sfx.zurap9dhYL8fyb9Y.O82EBwIAhODSJJk9g7XoyY2vbfUpgzm2',NULL,NULL,'admin',14510);
 
 INSERT INTO user_role (user_id,role_id)
-VALUES (1,1);
+VALUES (14510,14505);
